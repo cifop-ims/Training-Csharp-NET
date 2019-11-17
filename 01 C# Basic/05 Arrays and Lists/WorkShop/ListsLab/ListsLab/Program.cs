6b7418f9f9d39fe79588684998aa5d39fda6639d
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2019-11-17 08:24:49 +0100

    Initial commit

---
+++ 01 C# Basic/05 Arrays and Lists/WorkShop/ListsLab/ListsLab/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>() { 1, 2, 3, 4 };
+            list.Add(1);
+            list.AddRange(new int[3] { 5, 6, 7 });
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Count :" + list.Count);
+
+            Console.WriteLine("index of 1 :" + list.IndexOf(1));
+            Console.WriteLine("last index of 1 :" + list.LastIndexOf(1));
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == 1)
+                    list.Remove(list[i]);
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
